38a5f8a673a3b26b086a400c89e35c539e4918d2
kbl212 <kyle.lauritzen@gmail.com>
2016-03-04 22:50:59 -0700

    drop-downs populating correctly, ng-disabled working correctly on Submit button

--- client/js/controllers/mainCtrl.js
+++ client/js/controllers/mainCtrl.js
@@ -2,7 +2,13 @@ var app = angular.module('know-your-reps');
 
 
 app.controller('mainCtrl', function($scope, mainService) {
-        
-    var stateInitials = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"];
     
+    $scope.stateInitials = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"];
+    
+    $scope.repTypes = ["Representative", "Senator"];
+    
+    //ng-disabled expression...HERE
+    
+    $scope.selectedRepType = "";
+    $scope.selectedState = "";
 });
